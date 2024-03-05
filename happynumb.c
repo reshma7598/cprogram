850419e9de04ec41c94f91d18d6939bd20221b86
reshma7598 <reshmarajanr136@gmail.com>
2024-03-05 11:20:28 +0530

    second commit

--- happynumb.c
+++ happynumb.c
@@ -4,7 +4,7 @@ int main(){
     int number =19;
     int i;
     for(i=1;i<10;i++){
-        printf("%d ^2 + 1^2 %d = %d\n",number,i,number * i);
+        printf("%d ^2 + 1^2 %d = %d\n",number+i,i,number * i);
     }
     return 0;
 }
